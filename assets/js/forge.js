d5a5ea674d3a68f094d049e3c2f944c680035e29
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-08-19 13:18:47 +0200

    Add email validation in form signup

--- assets/js/forge.js
+++ assets/js/forge.js
@@ -5,12 +5,16 @@ $(function() {
         url = "/add_to_mailchimp.php?" + "email=" + email_value + "&name=" + name,
         signup_response =
           "Thank you, " + name + "!<br>\
-          Welcome to the inner circle of super people who will be informed about forge events.";
-
-    jQuery.post(url, function(rslt){
-      $('#mc-embedded-subscribe-form').remove();
-      $('#top').append('<p>' + signup_response + '</p>');
-    });
+          Welcome to the inner circle of super people who will be informed about Forge events.";
+    
+    if(email_value.match(/\S+@\S+/)) {
+      $.post(url, function(rslt){
+        $('#error-response, #mc-embedded-subscribe-form').remove();
+        $('#top').append('<p>' + signup_response + '</p>');
+      }); 
+    } else {
+      $('#mc-embedded-subscribe-form').before('<p id="error-response">Sorry, but that email won\'t work. Please check your mail address and try again.</p>');
+    }
 
     return false;
   });
