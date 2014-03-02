58dc3a5c26719c477265da7b701704d23d4b23a4
Casper Klenz-Kitenge <hi@cabgfx.com>
2014-03-02 22:50:37 +0100

    Content + footer for Forge no. 2

--- assets/js/forge.js
+++ assets/js/forge.js
@@ -1,4 +1,18 @@
 $(function() {
+  // BigVideo for non-touch devices
+  // var BV = new $.BigVideo({container: $('#top')});
+  // BV.init();
+  
+  // if (!Modernizr.touch) {
+  //   $('#big-video-wrap').hide();
+  // 
+  //   BV.show('//s3-eu-west-1.amazonaws.com/forgecph/Forge.mp4', {ambient: true});
+  // 
+  //   BV.getPlayer().on('loadedmetadata', function() {
+  //     $('#big-video-wrap').fadeIn(1000);
+  //   })
+  // }
+
   var signup_spinner = Ladda.create( document.querySelector( 'button' ) );
 
   // Post signups to Mailchimp
