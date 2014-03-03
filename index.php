ee9afb7db040b92d3ccf40d2de805ea6ac8f222a
Casper Klenz-Kitenge <hi@cabgfx.com>
2014-03-03 02:01:26 +0100

    Move boilerplate to separate files

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php $description = 'We start the sort of activities that we\’d like to participate in ourselves.'; ?>
+<?php include 'templates/head'; ?>
+
+<body>
+  <header role="banner" class="masthead">
+    <div class="container" id="top">
+      <h1 class="logo"><a href="/">Forge</a></h1>
+    
+      <p class="lead">
+        The next Forge event is coming <a href="https://www.facebook.com/events/591070834317509/?ref=22">June 20th, 2014.</a><br>
+        Sign up below to be notified when tickets go on sale.
+      </p>
+
+      <form action="http://forgecph.us7.list-manage.com/subscribe/post?u=f7462056ceb003d10ff72c090&amp;id=1752b37bfa" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="signup" novalidate>
+        <input type="text" value="" id="name" name="FNAME" required placeholder="Tell us your name">
+        <input type="email" required placeholder="And your email" value="" name="EMAIL" id="email">
+
+         <button class="ladda-button" data-style="zoom-in" type="submit" id="submit" data-spinner-color="#004835">
+           <span class="ladda-label">Ok</span>
+         </button>
+      </form>
+      <!--End mc_embed_signup-->
+    </div>
+  </header>
+
+  <main role="main" class="event-1-video">
+    <div class="container container-copy">
+      <h2>What is Forge?</h2>
+      <p class="lead">
+        It’s a Copenhagen Con-Up&hellip; a Meet-Ference&hellip;<br>
+        You better just watch this video.
+      </p>
+
+      <div class="video">
+        <iframe src="//player.vimeo.com/video/85478724?title=0&amp;byline=0&amp;portrait=0&amp;color=c73932" width="1000" height="562" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+    </div>
+  </main>
+
+  <section role="main" class="content">
+    <div class="container container-copy">
+      <h3 id="who-we-are">Who we are</h3>
+
+      <p class="col-1">
+          We’re a band of passionate creative professionals who long for a place to come together and share stories.
+          We find it astonishing that we’ve got so many talented creatives working or studying in and around Copenhagen,
+          yet so few common grounds. We’re not professional event makers, we won’t promise you fancy networking or the latest in conference trends.
+      </p>
+      <p class="col-2">
+        We start the sort of activities that we’d like to participate in ourselves.
+        We want to bring together the hardworking pixel pushers, the savvy entrepreneurs, the diligent students and the creative aficionados.
+        We have a wide spectrum of creatives in this city, locked up in companies, slaving away in startups or stuck in school.
+        We need to get out more, we have so much to learn.
+      </p>
+
+      <div class="initiators">
+        <p class="initiator">
+          <img src="assets/img/pete-lacey.png" alt="">
+          <strong>Pete Lacey</strong>
+          Lead Designer at Podio. Fond of badgers.
+          <a href="https://www.twitter.com/chopeh" title="Pete's twitter profile" target="_blank">@chopeh</a>
+        </p>
+        <p class="initiator">
+          <img src="assets/img/martin-leblanc.png" alt="">
+          <strong>Martin LeBlanc</strong>
+          Founder &amp; CEO of Iconfinder.
+          <a href="https://www.twitter.com/iconfinder" title="Martin's twitter profile" target="_blank">@iconfinder</a>
+        </p>
+        <p class="initiator">
+          <img src="assets/img/michael-flarup.png" alt="">
+          <strong>Michael Flarup</strong>
+          Founder and lead designer at Robocat &amp; Pixelresort.
+          <a href="https://www.twitter.com/flarup" title="Michael's twitter profile" target="_blank">@flarup</a>
+        </p>
+        <p class="initiator">
+          <img src="assets/img/casper-klenz-kitenge.png" alt="">
+          <strong>Casper Klenz-Kitenge</strong>
+          Designer and founder at Duke UX. Coffee lover.
+          <a href="https://www.twitter.com/cabgfx" title="Casper's twitter profile" target="_blank">@cabgfx</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+<?php include 'templates/footer'; ?>
