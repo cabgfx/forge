474ba41360a2b75d86cb2fdb52d6acaf437b4ad9
Casper Klenz-Kitenge <hi@cabgfx.com>
2014-03-04 13:59:54 +0100

    Fix issues with nav on touch screens

--- assets/js/forge.js
+++ assets/js/forge.js
@@ -1,18 +1,16 @@
 $(function() {
   // BigVideo for non-touch devices
-  if ($('body').hasClass('frontpage')) {
+  if ((!Modernizr.touch) && ($('body').hasClass('frontpage'))) {
     var BV = new $.BigVideo({container: $('#top')});
     BV.init();
   
-    if (!Modernizr.touch) {
-      $('#big-video-wrap').hide();
+    $('#big-video-wrap').hide();
   
-      BV.show('//s3-eu-west-1.amazonaws.com/forgecph/Forge2.mp4', {ambient: true});
+    BV.show('//s3-eu-west-1.amazonaws.com/forgecph/Forge2.mp4', {ambient: true});
   
-      BV.getPlayer().on('loadedmetadata', function() {
-        $('#big-video-wrap').fadeIn(1000);
-      })
-    }
+    BV.getPlayer().on('loadedmetadata', function() {
+      $('#big-video-wrap').fadeIn(1000);
+    });
   };
 
   var signup_spinner = Ladda.create( document.querySelector( 'button' ) );
