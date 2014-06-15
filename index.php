cc7426c5cf3096768db2e4940d0bc72268993c92
Casper Klenz-Kitenge <hi@cabgfx.com>
2014-06-15 11:05:35 +0200

    Update tix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php $description = 'We start the sort of activities that we’d like to participate in ourselves.'; ?>
 <?php $ticket_url = 'http://billetto.dk/forge-2' ?>
-<?php $ticket_availability = '52' ?>
+<?php $ticket_availability = '47' ?>
 
 <?php include 'templates/head.php'; ?>
 
